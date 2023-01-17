d701fe1f2bffa20458b9f5452539d09f7c707c34
Антон Селифанов <tatoxer@gmail.com>
2023-01-17 17:20:35 +0300

    added brain-progression game

--- src/cli.js
+++ src/cli.js
@@ -4,6 +4,7 @@ import _ from 'lodash';
 
 const askUserName = () => {
     let name = readlineSync.question('May I have your name?\n');
+    name.length === 0 ? name = 'noname' : name;
     name = _.upperFirst(name);
     console.log(`Hello, ${name}!`);
     return name;
