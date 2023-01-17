cebf03f8d9eb0a633cda25833e4a8d3551b79060
Антон Селифанов <tatoxer@gmail.com>
2023-01-17 10:39:26 +0300

    added brain-calc game

---
+++ bin/brain-calc.js
@@ -0,0 +1,46 @@
+#!/usr/bin/env node
+import readlineSync from 'readline-sync';
+import greeting from './brain-games.js';
+import {checkAndCountCorrectAnswers} from "./brain-games.js";
+
+const chooseOperation = () => {
+    const expressions = ['+', '-', '*'];
+    const min = 0;
+    const max = 3;
+    return expressions[Math.floor(Math.random() * (max - min) + min)];
+};
+
+const applyOperation = (number1, number2, operation) => {
+    let result;
+    if (operation === '+') {
+        result = number1 + number2;
+    } else if (operation === '-') {
+        result = number1 - number2;
+    } else {
+        result = number1 * number2;
+    }
+    return result;
+};
+
+const brainCalc = () => {
+    const name = greeting();
+    console.log('What is the result of the expression?');
+    let counter = 0;
+
+    while (counter < 3) {
+        const number1 = Math.floor(Math.random() * 100);
+        const number2 = Math.floor(Math.random() * 10);
+        const operation = chooseOperation();
+        const correctResult = applyOperation(number1, number2, operation);
+
+        console.log(`Question: ${number1} ${operation} ${number2}`);
+        const playerResult = Number(readlineSync.question('Your answer: '));
+        counter = checkAndCountCorrectAnswers(playerResult, correctResult, counter);
+
+        if (counter === 3) {
+            console.log(`Congratulations, ${name}!`);
+        }
+    }
+};
+
+brainCalc();
--- bin/brain-games.js
+++ bin/brain-games.js
@@ -5,4 +5,19 @@ const greeting = () => {
     console.log('Welcome to the Brain Games!');
     return askUserName();
 };
+
+const checkAndCountCorrectAnswers = (userResult, correctResult, counter) => {
+    if (userResult === correctResult) {
+        console.log('Correct!');
+        // eslint-disable-next-line no-param-reassign
+        counter += 1;
+    } else {
+        // eslint-disable-next-line no-param-reassign
+        counter = 4;
+        console.log(`Wrong!, correct answer is '${correctResult}'`);
+    }
+    return counter;
+};
+
 export default greeting;
+export {checkAndCountCorrectAnswers};
